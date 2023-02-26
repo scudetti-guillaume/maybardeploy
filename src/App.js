09f39d768af769f52821b92d836ad8679925ff95
scudetti-guillaume <124565246+scudetti-guillaume@users.noreply.github.com>
2023-02-26 19:16:30 +0100

    FIX deploy

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path='/carte' element={<Carte />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/condition' element={<Conditions />} />

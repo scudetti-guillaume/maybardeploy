df74b3df99c70d4b645c80baf27ff3a85789f26b
scudetti-guillaume <124565246+scudetti-guillaume@users.noreply.github.com>
2023-02-26 19:23:43 +0100

    TEST deploy

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/carte' element={<Carte />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/condition' element={<Conditions />} />
+          <Route path="https://scudetti-guillaume.github.io/maybardeploy/" element={<Home />} />
+          <Route path='https://scudetti-guillaume.github.io/maybardeploy/carte' element={<Carte />} />
+          <Route path='https://scudetti-guillaume.github.io/maybardeploy/contact' element={<Contact />} />
+          <Route path='https://scudetti-guillaume.github.io/maybardeploy/condition' element={<Conditions />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
